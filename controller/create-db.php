d78002e45896ed4f22c1fd4a468ae837b60b8dc6
Christopher Williams <cwilks225@gmail.com>
2015-01-28 14:03:14 -0800

    1st Commit

--- controller/create-db.php
+++ controller/create-db.php
@@ -1,13 +1,16 @@
 <?php
-    require_once("../model/database.php");
+    require_once(__DIR__ ."/../model/database.php");
+    //__DIR__. fixes the messed up path on our php file.
 
-    $connection= new msqli($host, $username, $password);
+    $connection= new mysqli($host, $username, $password);
 
     if($connection->connect_error){
        die("Error: ". $connection->connect_error); 
+       //If the program has a connection error the program will die
     }
     else{
         echo "Success" . $connection->host_info;
+        //If the program doesn't have a connection error it will give you information to the host
     }
     
     $connection->close();
--- index.php
+++ index.php
@@ -11,7 +11,7 @@ and open the template in the editor.
     </head>
     <body>
         <?php
-          echo "Web Server is Running Locally" ;
+            require_once(__DIR__ . "/controller/create-db.php");
         ?>
     </body>
 </html>
