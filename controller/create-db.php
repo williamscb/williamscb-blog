4d0aa58f4365e078db4bfa25a46271a1981bfd7d
Christopher Williams <cwilks225@gmail.com>
2015-01-29 14:03:23 -0800

    2nd Commit

--- controller/create-db.php
+++ controller/create-db.php
@@ -3,14 +3,36 @@
     //__DIR__. fixes the messed up path on our php file.
 
     $connection= new mysqli($host, $username, $password);
+    // Connects you to mysqli and gives you the host, username, and password
 
     if($connection->connect_error){
        die("Error: ". $connection->connect_error); 
        //If the program has a connection error the program will die
     }
-    else{
-        echo "Success" . $connection->host_info;
-        //If the program doesn't have a connection error it will give you information to the host
+    
+    $exists = $connection->select_db($database);
+    
+    if(!$exists){
+      $query = $connection->query("CREATE DATABASE $database");
+      
+      if($query) {
+          echo "Successfully created database" . $database;
+      }
     }
+    else {
+        echo "Database has already exists.";
+    }
+    
+    $query = $connection->query("CREATE TABLE posts ("
+            . "id int(11) NOT NULL AUTO_INCREMENT,"
+            . "title varchar (255) NOT NULL,"
+            . "post text NOT NULL,"
+            . "PRIMARY KEY(id))"); 
+    // Creates a query that creates a table that needs to have an id,title, and post
+    
+    if($query){
+        echo "Successfully created table: posts";
+    }
+   
     
     $connection->close();
