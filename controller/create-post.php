1990e78a749b32038a76fa57143b133fd5c300b8
Christopher Williams <cwilks225@gmail.com>
2015-02-03 13:14:40 -0800

    4th Commit

--- controller/create-post.php
+++ controller/create-post.php
@@ -1,4 +1,8 @@
 <?php
+    require_once(__DIR__ . "/../model/database.php");
+    
+    $connection = new mysqli($host, $username, $password, $database);
+    
  $title = filter_input(INPUT_POST, "title", FILTER_SANITIZE_STRING);
  //A mechanism used to protect our database//
  $post = filter_input (INPUT_POST, "post", FILTER_SANITIZE_STRING);
@@ -6,4 +10,6 @@
  echo "<p>Titile: $title</p>";
  echo "<p>Post: $post</p>";
  
+ 
+ $connection->close();
 
