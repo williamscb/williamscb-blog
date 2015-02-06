43fb0aeeabbbddec93ef6d3117f406d89374e7cb
Christopher Williams <cwilks225@gmail.com>
2015-02-06 13:53:09 -0800

    another commit

--- model/Database.php
+++ model/Database.php
@@ -1,5 +1,36 @@
 <?php
 class Database {
+   
+    private $connection;
+    private $host;
+    private $username;
+    private $password;
+    private $database;
     
+    public function __construct($host, $username, $password, $database){
+        $this->host = $host;
+        $this->username = $username;
+        $this->password = $password;
+        $this->database = $database;
+    }
+    
+    public function openConnection(){
+       $this->connection = new mysqli($this->host, $this->username, $this->password, $this->database);
+       
+       if ($connection->connect_error) {
+            die("<p>Error: " . $this->connection->connect_error . "</p>");
+            //If the program has a connection error the program will die
+        }
+    }
+    
+    public function closeConnection(){
+        if(isset($this->connection)){
+           $this->connection->close(); 
+        }
+    }
+    
+    public function query($string){
+        
+    }
 }
 
